e148d2b343bafcf7ae913733ad60d6c1426f5566
đat thanh <nguyenthanhdatt2001@gmail.com>
2023-12-25 22:01:32 +0700

    buoi2

--- C#.net1/Lesson2/VariableDatatype/Program.cs
+++ C#.net1/Lesson2/VariableDatatype/Program.cs
@@ -1,4 +1,60 @@
 ﻿
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine();
+//string name = " thanhdat";
+//int age = 21;
+//int na = 32;
+//int chi = 12;
+//int oc = 4;
+
+// viet ten tuoi va in ta 
+//Console.WriteLine(" input the name ");
+//string name = Console.ReadLine();
+
+//Console.WriteLine(" input the age ");
+//string age = Console.ReadLine();
+
+//Console.WriteLine(" input the address ");
+//string address = Console.ReadLine();
+//Console.WriteLine($" name:{name}, age:{age}, adress:{address}");
+//Console.WriteLine(string.Format("name:{0}, age:{1}, adress:{2}", name, age, address));
+
+//Console.WriteLine(name);
+//Console.WriteLine(age);
+//Console.WriteLine(address);
+//Console.ReadLine();
+long number1 = 10;
+int number2 = 25;
+long number3 = number2;
+int number4 = (int)number1;
+object objNumber4 = number4;
+int number5 = (int)objNumber4;
+
+double doubleValue = 10.5;
+float floatValue = 6.7f;
+decimal decimalValue = 6.7M;
+float floatValuel = (float)doubleValue;
+char letter ='a';
+bool check = true;
+bool checkl = 5> 6;
+DateTime today = DateTime.Now;
+int result = 10 / 3;
+int resultl = 10 % 3;
+double result2 = 10 / (double)3;
+int newNumber = 5;
+newNumber++;  // newnumber = newNumber +1
+int newNumber1 = 10;
+newNumber1 += 5; //newNumber =newNumber +5
+
+bool checkResult = 9 > 3 && 9 < 10;
+
+bool checkResult1 = 9 > 3 || 9 < 10;
+bool checkResult2 = 3==7 -4 ; // bang
+bool checkResult3 = 3!=7 -2 ;// khac
+
+
+Console.ReadLine();
+
+
+
+
+
+
