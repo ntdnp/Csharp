e6453f2a78faf5f9fa2e33b37261edbe6d2e6be6
đat thanh <nguyenthanhdatt2001@gmail.com>
2024-01-10 20:43:30 +0700

    if else

--- C#.net1/Lesson3/LearnIfElse/Program.cs
+++ C#.net1/Lesson3/LearnIfElse/Program.cs
@@ -1,6 +1,6 @@
 ﻿using LearnIfElse;
 Service service = new Service();
 string result = service.EvaluateStudent(4);
-
+bool check = service.CheckLunarYear(2020);
 Console.ReadKey();
 
--- C#.net1/Lesson3/LearnIfElse/Service.cs
+++ C#.net1/Lesson3/LearnIfElse/Service.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace LearnIfElse
 {
@@ -13,7 +14,7 @@ namespace LearnIfElse
         public string EvaluateStudent(double score)
         {
             string result = " ";
-            if (score==5)
+            if (score == 5)
             {
                 result = " average";
             }
@@ -30,6 +31,58 @@ namespace LearnIfElse
             }
             return result;
         }
+        // viet ham xem nam do co phai nam nhuận hay ko 
+        // neu nam do chia het cho 100 thi phai chia het cho 400
+        // neu nam do ko chia het cho 100 thi phai chia het cho 4
+        public bool CheckLunarYear(int year)
+        {
+            if (year < 0)
+            {
+                return false;
+            }
+
+            if (year % 100 == 0)
+            {
+                if (year % 400 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (year % 4 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+
+
+        }
+        public bool CheckLunarYear1(int year)
+        {
+            if ((year % 100 == 0 && year % 400 == 0) || (year % 100 != 0 && year % 4 == 0))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckLunarYear2(int year)
+        {
+            return (year % 100 == 0 && year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
+        }
+
+        public bool CheckLunarYear3(int year)
+            => (year % 100 == 0 && year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
 
     }
 }
