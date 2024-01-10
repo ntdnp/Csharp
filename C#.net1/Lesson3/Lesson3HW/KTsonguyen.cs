45f5f5dcb9893e34b63de8e4546b1f813fea6556
đat thanh <nguyenthanhdatt2001@gmail.com>
2024-01-10 16:34:14 +0700

    lesson3HW

---
+++ C#.net1/Lesson3/Lesson3HW/KTsonguyen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3HW
+{
+    internal class KTsonguyen
+    {
+        static void Main()
+        {
+            Console.Write("Nhap mot so nguyen: ");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                CheckNumberType(number);
+            }
+            else
+            {
+                Console.WriteLine("khong hop le ");
+            }
+
+           
+            Console.ReadKey();
+        }
+
+        static void CheckNumberType(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine("so duong .");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("So am.");
+            }
+            else
+            {
+                Console.WriteLine("So bang 0 .");
+            }
+        }
+
+    }
+   
+}
