89139fc1f505e8f840ec4dbc359eccc96d419839
đat thanh <nguyenthanhdatt2001@gmail.com>
2024-01-15 14:30:37 +0700

    lesson4HW

--- C#.net1/Lesson4/lesson4HW/Program.cs
+++ C#.net1/Lesson4/lesson4HW/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using lesson4HW;
+Service service = new Service();
+string message1 = service.TriangleTest(3, 3, 3);
+string message2 = service.TriangleTest(3, 4, 4);
+string message3 = service.TriangleTest(3, 4, 5);
+string message4 = service.TriangleTest(3, 5, 9);
+Console.ReadKey();
+
---
+++ C#.net1/Lesson4/lesson4HW/Service.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson4HW
+{
+    internal class Service
+    {
+        /// Viết hàm nhận vào 3 cạnh của 1 tam giác ( kiểu int) và trả ra xem tam giác đó là cân, vuông hay không phải là tam giác
+
+        public string TriangleTest(int a,int b,int c)
+        {
+            try 
+           
+            {
+                if (a + b <= c || a + c <= b || b + c <= a)
+                {
+                    return "Khong phai tam giac ";
+
+                }
+                else if (a == b && b == c)
+                {
+                    return "La tam giac deu";
+                }
+                else if (a == b || b == c || a == c)
+                {
+                    return "La tam giac can";
+                }
+                else if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a)
+                {
+                    return "La tam giac vuong";
+                }
+                else
+                {
+                    return "La tam giac thuong";
+                }
+            }
+            catch 
+            
+            {
+                return "co loi ";
+            }
+        }
+    }
+}
